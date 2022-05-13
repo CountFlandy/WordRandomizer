d8a62166286230af7e4773fbb36e4b2196198f1e
CountFlandy <32300491+CountFlandy@users.noreply.github.com>
2022-05-13 14:18:00 -0400

    See ChangeLog.txt
    - Results printed to text file
    - New Algorithim
    - Fallback Timer
    - Continue Option

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ThreeLetterWord
+# WordRandomizer
 WHAT IT DOES:
 
 Enter in ANY word of any length (preferably larger than 2 letters, otherwise results are boring) and it will spit out every possible word that can be made 
@@ -13,9 +13,11 @@ and I found this on my hard drive.
 CAN IT BE IMPROVED?:
 
 Yes, there are two TODO's I left, as well as a few other ideas I'd like to improve eventually:
-1: Implement a way for generated "words" to be compared to a dictionary of the English Language, so it only can output actual English words, and not nonsense.
-2: Allow for results to be of any length, as opposed to always words of the given length.
-3: Output results to a text document, so they are easily viewed and saved.
-4: An actual UI could be nice, but not needed. That's not something I do well, however.
-5: I'm sure my algorithims are bad. They likely can be improved. 
-6: I didn't bother to clean up the code for this first release. So a code cleanup.
+1: Textfile outputs could be actually organized, as opposed to "REAL WORD - xxxx" and "FAKE WORD - xxxx"
+2: I'm sure my algorithims are bad. My math is possibly wrong. Not good at math, so double check it if you suspect something is off..
+3: An actual UI could be nice, but not needed. That's not something I do well, however.
+
+WILL I IMPROVE IT ANYMORE FROM HERE?:
+Unless I decide to port this to something with a UI, I doubt it. Unless you report a bug, then I'll try to fix it the best I can.
+
+
--- ThreeLetterWord.cs
+++ ThreeLetterWord.cs
@@ -6,126 +6,251 @@ using System.Reflection;
 
 namespace RandomizedWord
 {
-
-    //TODO: Allow for items randomized to be of any # of letters instead of only the length. This can improve found words. Will need to improve loop however.
-    //TODO: Add an option for displaying of all "Fake" words as well as all "Real" words. Likely will require changing how they are displayed for user?
-    //TODO: Remove user caps, and trim white space
-    //TODO: Capitolize first letter of the "Fake" word
-
     class ThreeLetterWord
     {
         static void Main(string[] args)
         {
-            string word;
-            //To later print the correct value of real words to user
-            int realWordCount = 0;
-            bool dupecheck = true;
-
-            string wordRead;
-            Console.WriteLine("Enter a 5 letter word and all possible 3 letter combinations will be retrieved.");
-            word = Console.ReadLine();
-            Console.WriteLine("");
-
-            //Loop to check for answers smaller than size
-            if (word.Length < 5)
+            string userDifferentWord = "yes";
+            while (userDifferentWord == "yes")
             {
-                for (int i = 0; i < 1;)
+                string userWord;
+                int realWordCount = 0; //To later print the correct value of real words to user
+                string optionFakeWords;
+                bool fakeWords = true;
+                bool goAgain = false;
+                string inputFilePath = "null";
+
+                Console.WriteLine("Enter a 5 letter word and all possible 3 letter combinations will be retrieved.");
+                userWord = Console.ReadLine();
+                Console.WriteLine("");
+                userWord.Trim();
+                userWord.ToLower();
+
+                //Loop to check for answers smaller than size
+                if (userWord.Length < 5)
                 {
-                    if (word.Length < 6)
+                    for (int i = 0; i < 1;)
                     {
-                        i++;
+                        if (userWord.Length < 6)
+                        {
+                            i++;
+                        }
+                        Console.WriteLine("Error, word length too small. Try again.");
+                        Console.WriteLine("Enter a 5 letter word and all possible 3 letter combinations will be retrieved.");
+                        userWord = Console.ReadLine();
+                        Console.WriteLine("");
                     }
-                    Console.WriteLine("Error, word length too small. Try again.");
-                    Console.WriteLine("Enter a 5 letter word and all possible 3 letter combinations will be retrieved.");
-                    word = Console.ReadLine();
+                }
+
+                Console.WriteLine("Do you want to display only real words?");
+                Console.WriteLine("Example: (Word is: House) uohse would be filtered out.");
+                Console.WriteLine("");
+                optionFakeWords = Console.ReadLine();
+                optionFakeWords.Trim();
+                optionFakeWords.ToLower();
+                if (optionFakeWords == "yes" || optionFakeWords == "Yes" || optionFakeWords == "ye" || optionFakeWords == "y" || optionFakeWords == "Y")
+                {
+                    fakeWords = true;
+                }
+                else if (optionFakeWords == "no" || optionFakeWords == "No" || optionFakeWords == "n" || optionFakeWords == "N")
+                {
+                    fakeWords = false;
+                }
+                else
+                {
+                    Console.WriteLine("Error: Neither yes or no were detected. Defaulting to fake word filtering ON. No fake words will be sent to user.");
                     Console.WriteLine("");
                 }
-            }
 
-            //Check for if user wants duplicate filtering on or off.
-            Console.WriteLine("Do you want to filter out duplicates? (Yes/No)");
-            Console.WriteLine("Examples: aaa, aab, abb");
-            Console.WriteLine("");
-            wordRead = Console.ReadLine();
-            if (wordRead == "yes" || wordRead == "Yes" || wordRead == "ye" || wordRead == "y" || wordRead == "Y")
-            {
-                dupecheck = true;
-            }
-            else if (wordRead == "no" || wordRead == "No" || wordRead == "n" || wordRead == "N")
-            {
-                dupecheck = false;
-            }
-            else
-            {
-                Console.WriteLine("Error: Neither yes or no were detected. Defaulting to duplicate filtering ON. No Duplicates will be sent to user.");
+                Console.WriteLine("Would you like to write the results into a text file for later reading?");
+                Console.WriteLine("Yes/y or No/n");
+                Console.WriteLine();
+                string saveFile = Console.ReadLine();
+                saveFile.Trim();
+                saveFile.ToLower();
+                if(saveFile == "yes" || saveFile == "y" || saveFile == "ye")
+                {
+                    if (File.Exists(inputFilePath + ".txt"))
+                    {
+                        File.Delete(inputFilePath);
+                    }
+                    inputFilePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                }
+                else if (saveFile == "no" || saveFile == "n" || saveFile == "noo")
+                {
+                    Console.WriteLine("Nothing will be written to a text file.");
+                }
+                StreamWriter writeFile = new StreamWriter(userWord + ".txt");
+
+                int wordLength = userWord.Length; //Get length of word above
+                int minWordLength = 3;
+                List<string> wordList = new List<string>(); //Create string we'll use below
+
+                for (int i = 0; i < wordLength; i++) //Add the user generated word to the list
+                {
+                    wordList.Add(userWord.Substring(i, 1));
+                }
+
+                List<String> shuffleWordList = new List<String>();
+                List<String> compareWordList = new List<String>();
+                compareWordList.Add(userWord);
+                string compareString = "";
+                Random rnd = new Random();
+                int k = (rnd.Next(0, 100));
+                int wL = wordLength;
+                int mWL = minWordLength;
+                bool suddenlyBreak = false;
+                int timer = 0;
+                int maxPassesEqu = ((userWord.Length - mWL + 1) *  userWord.Length * 26);
+                shuffleWordList = wordList;
+
+                for (int i = 0; i <= maxPassesEqu;)
+                {
+                    if (wordList.Count != wordLength)
+                    {
+                        wordList.Clear();
+                        for (int r = 0; r < wordLength; r++) //Add the user generated word to the list
+                        {
+                            wordList.Add(userWord.Substring(r, 1));
+                        }
+                    }
+                    if (shuffleWordList.Count != wordLength)
+                    {
+                        shuffleWordList.Clear();
+                        for (int e = 0; e < wordLength; e++) //Add the user generated word to the list
+                        {
+                            shuffleWordList.Add(userWord.Substring(e, 1));
+                        }
+                    }
+                    for (int q = 0; q < 1;) //This part finds the "words"
+                    {
+                        if (shuffleWordList.Count != wordLength)
+                        {
+                            shuffleWordList.Clear();
+                            for (int e = 0; e < wordLength; e++) //Add the user generated word to the list
+                            {
+                                shuffleWordList.Add(userWord.Substring(e, 1));
+                            }
+                        }
+
+                        wL = wordLength;
+                        mWL = minWordLength;
+                        timer = timer + 1;
+                        if (timer > maxPassesEqu)
+                        { //just in case my math is wrong this is a safeway to abort it.
+                            suddenlyBreak = true;
+                            i = maxPassesEqu + 1;
+                            q = 2;
+                        }
+
+                        while (wL > 1)
+                        {
+                            k = (rnd.Next(0, wL));
+                            wL--;
+                            string value = shuffleWordList[k];
+                            shuffleWordList[k] = shuffleWordList[wL];
+                            shuffleWordList[wL] = value;
+                        }
+                        wL = wordLength;
+                        int r = rnd.Next(mWL, wL);
+                        while (wL > r)
+                        {
+                            if (shuffleWordList.Count < mWL)
+                            {
+                                q = 2;
+                                suddenlyBreak = true;
+                            }
+                            else
+                            {
+                                shuffleWordList.RemoveAt(r);
+                                wL--;
+                            }
+                        }
+                        if (!compareWordList.Contains(shuffleWordList.ToString()))
+                        {
+                            q++;
+                        }
+                    }
+                    if (suddenlyBreak == false)
+                    {
+                        //Creates a string that is used when compared against compareWordList
+                        foreach (var item in shuffleWordList)
+                        {
+                            compareString = compareString + item;
+                        }
+
+                        if (compareString.Length <= wordLength && compareString.Length >= minWordLength && !compareWordList.Contains(compareString))
+                        {
+                            string dupeWord = "";
+                            dupeWord = compareString;
+                            DictionaryCheck localDict = new DictionaryCheck();
+                            dupeWord = localDict.EngDictionary(dupeWord);
+                            if (dupeWord == compareString)
+                            {
+                                Console.WriteLine(compareString);
+                                writeFile.WriteLine("REAL WORD: " + compareString.ToString());
+                                realWordCount++;
+                            }
+                            else
+                            {
+                                if (fakeWords == false)
+                                {
+                                    writeFile.WriteLine("FAKE WORD: " + compareString.ToString());
+                                    Console.WriteLine(compareString);
+                                }
+                            }
+                            //Add the string that passed the above comparision to the list so that it may no longer be taken.
+                            compareWordList.Add(compareString);
+                            i++;
+                        }
+                        //Clear string so we get a fresh start above
+                        compareString = "";
+                    }
+                }
+
+                if (realWordCount == 0)
+                {
+                    Console.WriteLine(compareWordList.Count + " entries were found. None of them were real words");
+                }
+                if (realWordCount > 0)
+                {
+                    Console.WriteLine(compareWordList.Count + " entries were found. Out of these " + realWordCount + " entries were real words.");
+                }
+
                 Console.WriteLine("");
-            }
-            //End checking for user duplicate filtering opinion.
+                Console.WriteLine("Would you like try a different word?");
+                Console.WriteLine("Yes/No");
 
+                userDifferentWord = Console.ReadLine();
+                userDifferentWord = userDifferentWord.Trim();
+                userDifferentWord = userDifferentWord.ToLower();
 
-            int wordLength = word.Length; //Get length of word above
-            List<string> wordList = new List<string>(); //Create string we'll use below
-
-            for (int i = 0; i < wordLength; i++) //Add the user generated word to the list
-            {
-                wordList.Add(word.Substring(i, 1));
-            }
-
-
-            List<String> shuffleWordList = new List<String>();
-            List<String> compareWordList = new List<String>();
-            string compareString = "";
-            Random rnd = new Random();
-
-            //This whole loop might need some improvement but it works
-            for (int i = 0; i <= word.Length * word.Length;)
-            {
-                shuffleWordList = wordList.OrderBy(a => rnd.Next()).ToList();
-
-                //Creates a string that is used when compared against compareWordList
-                foreach (var item in shuffleWordList)
+                if (userDifferentWord == "yes" || userDifferentWord == "ye" || userDifferentWord == "y")
                 {
-                    compareString = compareString + item;
+                    //return to default, then force a loop of program
+                    userWord = "null";
+                    compareWordList.Clear();
+                    realWordCount = 0;
+                    compareString = "null";
+                    fakeWords = true;
+                    //dupeWord = "null";
+                    //inputFilePath = "null";
+                    suddenlyBreak = false;
+                    shuffleWordList.Clear();
+                    wL = 0;
+                    wordList.Clear();
+                    mWL = 0;
+                    wordLength = 0;
+                    optionFakeWords = "null";
+                    userDifferentWord = "yes";
+                    Console.Clear();
                 }
-
-                if (!compareWordList.Contains(compareString))
+                else
                 {
-                    string WordListString = "";
-                    foreach (var item in shuffleWordList)
-                    {
-                        WordListString = WordListString + item;
-                    }
-
-                    string dupeWord = "";
-                    dupeWord = WordListString;
-                    DictionaryCheck localDict = new DictionaryCheck();
-                    dupeWord = localDict.EngDictionary(dupeWord);
-
-                    if (dupeWord == WordListString)
-                    {
-                        Console.WriteLine(WordListString);
-                        realWordCount++;
-                    }
-                    else { }
-
-                    //Add the string that passed the above comparision to the list so that it may no longer be taken.
-                    compareWordList.Add(WordListString);
-                    i++;
+                    Console.WriteLine("Goodbye");
+                    Environment.Exit(0);
                 }
-
-                //Clear string so we get a fresh start above
-                compareString = "";
             }
-            if (realWordCount == 0)
-            {
-                Console.WriteLine(compareWordList.Count + " entries were found. None of them were real words");
-            }
-
-            if (realWordCount > 0)
-            {
-                Console.WriteLine(compareWordList.Count + " entries were found. Out of these " + realWordCount + " entries were real words.");
-            }
-            Console.ReadLine();
         }
     }
 }
